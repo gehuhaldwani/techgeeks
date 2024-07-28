a655f96ece5446b48c587596aa4f4db7e7fcd235
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-07-28 12:53:09 +0530

    base path fix

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://gehuhaldwani.github.io",
-  base: "/",
+  base: "/techgeeks",
   author: "TechGeeks Team",
   profile: "",
   desc: "TechGeeks is the offical tech club of Graphic Era Hill University, Haldwani Campus.",
