5c6cb385b964d94473238c4f0b571c37d37289c5
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-08-24 08:54:35 +0000

    upated site base url generator

--- src/utils/generateUrlWithSiteBase.ts
+++ src/utils/generateUrlWithSiteBase.ts
@@ -1,12 +1,16 @@
 import { SITE } from "@config";
 
 export default function generateUrlWithSiteBase(url: string, siteOrigin: URL) {
-  if (url?.startsWith("http") || url?.startsWith("/" + SITE.base)) return url;
+  if (url?.startsWith("http")) return url;
 
   if (url?.startsWith(SITE.base))
-    return new URL("/" + url, siteOrigin.origin).href;
+    if (url?.startsWith(SITE.base + "/"))
+      return new URL(url, siteOrigin.origin).href;
+    else return new URL(`/${url}`, siteOrigin.origin).href;
 
   if (url?.startsWith("/"))
-    return new URL(`${SITE.base}${url}`, siteOrigin.origin).href;
+    if (url?.startsWith("/" + SITE.base))
+      return new URL(url, siteOrigin.origin).href;
+    else return new URL(`${SITE.base}${url}`, siteOrigin.origin).href;
   else return new URL(`${SITE.base}/${url}`, siteOrigin.origin).href;
 }
