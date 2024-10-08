2db3eacdd8c5b080e9616b2813b53ab534422a75
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-10-08 22:14:49 +0530

    fix opengraph

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Tech Geeks',
-  subtitle: 'Demo Site',
+  subtitle: 'Website of Tech Geeks Club, GEHU Haldwani',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 240,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
